d0681fb5c1bdb68ea0e05f506899a505abd9c0c5
helshey404 <olgha.shlykova@gmail.com>
2020-10-29 01:52:45 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,6 +16,41 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+
+    alph_b = []
+    c = 65
+    r = 65
+
+    for b1 in range(0, 26):
+        alph_b.append(chr(c))
+        c = c + 1
+
+    for b2 in range(26, 53):
+        alph_b.append(chr(r))
+        r = r + 1
+
+    alph_l = []
+    x = 97
+    y = 97
+    for b1 in range(0, 26):
+        alph_l.append(chr(x))
+        x = x + 1
+
+    for b2 in range(26, 53):
+        alph_l.append(chr(y))
+        y = y + 1
+
+    n = len(plaintext)
+    for j in range(0, n):
+        if plaintext[j] in alph_b or plaintext[j] in alph_l:
+            for b in range(0, 26):
+                if plaintext[j] == alph_b[b]:
+                    ciphertext += alph_b[b + shift]
+                if plaintext[j] == alph_l[b]:
+                    ciphertext += alph_l[b + shift]
+        else:
+            ciphertext += plaintext[j]
+
     return ciphertext
 
 
@@ -34,6 +69,41 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+
+    alph_b = []
+    c = 65
+    r = 65
+
+    for b1 in range(0, 26):
+        alph_b.append(chr(c))
+        c = c + 1
+
+    for b2 in range(26, 53):
+        alph_b.append(chr(r))
+        r = r + 1
+
+    alph_l = []
+    x = 97
+    y = 97
+    for b1 in range(0, 26):
+        alph_l.append(chr(x))
+        x = x + 1
+
+    for b2 in range(26, 53):
+        alph_l.append(chr(y))
+        y = y + 1
+
+    n = len(ciphertext)
+    for j in range(0, n):
+        if ciphertext[j] in alph_b or ciphertext[j] in alph_l:
+            for b in range(26, 52):
+                if ciphertext[j] == alph_b[b]:
+                    plaintext += alph_b[b - shift]
+                if ciphertext[j] == alph_l[b]:
+                    plaintext += alph_l[b - shift]
+        else:
+            plaintext += ciphertext[j]
+
     return plaintext
 
 

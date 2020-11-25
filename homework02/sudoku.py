4e7d05c118626ccbeef2403f4aedec00f40d2b83
Olga Shlykova <olgha.shlykova@gmail.com>
2020-11-25 20:11:37 +0300

    Update sudoku.py
    changed def group

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -36,13 +36,13 @@ def display(grid: tp.List[tp.List[str]]) -> None:
 def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     """
     Сгруппировать значения values в список, состоящий из списков по n элементов
-
     >>> group([1,2,3,4], 2)
     [[1, 2], [3, 4]]
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    result = [values[i:i + n] for i in range(0, len(values), n)]
+    return result
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
